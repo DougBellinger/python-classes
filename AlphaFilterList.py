a7a7e3114896e8a0865266960c87583743aa5da2
DougBellinger <doug.bellinger@gmail.com>
2023-09-10 11:50:25 -0400

    Updated

--- AlphaFilterList.py
+++ AlphaFilterList.py
@@ -6,12 +6,16 @@
 #-----------------------------------------------------------------------------
 import param
 import panel as pn
+import logging
 pn.extension()
+
+logger = logging.getLogger('panel.AlphaFilterList')
+
 class AlphaFilterList(param.Parameterized):
     letters = [chr(i) for i in range(ord('A'),ord('Z')+1)]
-    all = param.List(precedence=-1)
+    all = []
     name = param.String(default="Objects")
-    filter = param.ListSelector(objects=letters)
+    filter = param.ListSelector(default=[],objects=letters)
     selector = param.ListSelector(objects=['1','2'])
     width = param.Integer(default=100, precedence=-1)
     
@@ -19,29 +23,40 @@ class AlphaFilterList(param.Parameterized):
         return(sum(map(lambda x: 1 if x in w else 0, self.filter)))
 
     def match_all(self):
-        return [w for w in self.all if (self.count_matches(w)==len(self.filter))]
+        return [w for w in self.param.selector.objects if (self.count_matches(w)==len(self.filter))]
 
     def match_any(self):
-        return [w for w in self.all if (self.count_matches(w)>0)]
+        return [w for w in self.param.selector.objects if (self.count_matches(w)>0)]
    
     def match_none(self):
-        return [w for w in self.all if (self.count_matches(w)==0)]
+        return [w for w in self.param.selector.objects if (self.count_matches(w)==0)]
 
     def starts_with(self):
-        return [w for w in self.all if w[0] in self.filter]
+        return [w for w in self.param.selector.objects if w[0] in self.filter]
 
     def __init__(self, func=None, **params):
+        logger.debug(f"Creating AlphaFilterList {self.name}")
         self.function = func if func else self.starts_with
+        self.all = all if all else []
+        #self.filter=filter if filter else []
         super(AlphaFilterList, self).__init__(**params)
-        self.updateOptions() 
+        self.update_options() 
+
+    def reset_filter(self):
+        self.param.filter.objects = self.letters
+        self.param.filter = []
+        self.param.selector.objects = self.all
+        self.param.selector=[]
 
     @param.depends('all', watch=True)
-    def updateOptions(self):
+    def update_options(self):
+        logger.debug(f"{self.name} update filter")
         self.selector = []
         self.param.selector.objects = self.all
 
     @param.depends('filter', watch=True)
-    def updateFilter(self):
+    def update_filter(self):
+        logger.debug(f"{self.name} update filter")
         if (self.selector):
             self.param.selector.objects = self.selector + self.function(self)
         else:
@@ -51,6 +66,12 @@ class AlphaFilterList(param.Parameterized):
     def get_objects(self):
         return(self.param.selector.objects)
     
+    def drop_letters(self, to_drop):
+        logger.debug(f"{self.name} dropping letters {to_drop}")
+        if not to_drop:
+            return
+        self.param.filter.objects = [x for x in self.letters
+                                     if x not in to_drop]
     def __new_class(self, cls, **kwargs):
         return type(type(cls).__name__, (cls,), kwargs)
     
